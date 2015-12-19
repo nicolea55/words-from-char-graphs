996b18fb76656b79955d62ca8fac0b464d4f0910
Nicolae Babanu <nicu@inbox.ru>
2015-12-19 15:31:46 +0200

    Initial graph model and traversing logic to find out all words

---
+++ Graphs/Graphs.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+	public class Node
+	{
+		public List<Node> Neibours = new List<Node>();
+
+		public char CharVal = new char();
+
+		public Node(char c)
+		{
+			CharVal = c;
+		}
+	}
+
+	public class Graph
+	{
+		public List<Node> Nodes = new List<Node>();
+
+		public void AddNode(Node node)
+		{
+			if (!Nodes.Contains (node)) {
+				Nodes.Add (node);
+			}
+		}
+
+		public void AddEdge( Node node1, Node node2)
+		{
+			if (!node1.Neibours.Contains (node2)) {
+				node1.Neibours.Add (node2);
+			}
+
+			if (!node2.Neibours.Contains (node1)) {
+				node2.Neibours.Add (node1);
+			}
+		}
+
+		public void RemoveEdge(Node node1, Node node2)
+		{
+			if (node1.Neibours.Contains (node2)) {
+				node1.Neibours.Remove (node2);
+			}
+
+			if (node2.Neibours.Contains (node1)) {
+				node2.Neibours.Remove (node1);
+			}
+		}
+
+		public bool IsEdge(Node node1, Node node2)
+		{
+			bool result = node1.Neibours.Contains (node2) && node2.Neibours.Contains (node1);
+			return result;
+		}
+
+		public List<string> GetCombinations()
+		{
+			return MakeWords (Nodes);
+		}
+
+		// Current path
+		private List<Node> currentPath = null;
+
+		/// <summary>
+		/// Makes letter comginations out of some nodes.
+		/// </summary>
+		/// <returns>The words.</returns>
+		/// <param name="nodes">Nodes.</param>
+		/// <remarks>
+		/// This implementation is based on the assumption that each node is represented by a distinct Node object,
+		/// and the nodes already a part of a graph, with all edges being defined.
+		/// </remarks>
+		private List<string> MakeWords(List<Node> nodes)
+		{
+			List<string> words = new List<string> ();
+
+			bool iAmRoot = false;
+			// If this method is called first time, it is root of tree
+			if (currentPath == null) {
+				iAmRoot = true;
+			}
+
+			// For each node get the tree
+			foreach (Node node in nodes) {
+				// When traversing the first time in recursion path,
+				// each time the node is root, so reinit the checkedNodes list
+				if (iAmRoot) {
+					currentPath = new List<Node> ();
+				}
+
+				// If we did not met yet this node in current recursion path
+				if(!currentPath.Contains(node))
+				{
+					currentPath.Add (node);
+					string word = node.CharVal.ToString();
+					words.Add (word);
+					// for each tree path get the words list
+					// check each word
+					// if is word add it to list
+					// return print the list
+					if (node.Neibours != null) {
+						List<string> otherWords = MakeWords (node.Neibours);
+						foreach (string otherWord in otherWords) {
+							string newWord = word + otherWord;
+							words.Add (newWord);
+						}
+					}
+
+					// Remove the node we checked to allow other paths to it
+					if (currentPath.Contains (node)) {
+						currentPath.Remove(node);
+					}
+				}
+			}
+
+			// Set recycle current path so that other call knows it is starting over
+			if (iAmRoot) {
+				currentPath = null;
+			}
+
+			return words;
+		}
+	}
+
+
+}
+
---
+++ GraphyChars/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using Graphs;
+
+namespace Graphy
+{
+	class MainClass
+	{
+		/// <summary>
+		/// The ms dictionary of words.
+		/// </summary>
+		private static List<string> msDictionary = new List<string>();
+
+		/// <summary>
+		/// Determines if the word is found our dictionary.
+		/// </summary>
+		/// <returns><c>true</c> if is word the specified word; otherwise, <c>false</c>.</returns>
+		/// <param name="word">Word.</param>
+		private static bool IsWord(string word)
+		{
+			bool result = msDictionary != null ? msDictionary.Contains (word) : false;
+
+			return result;
+		}
+
+		/// <summary>
+		/// Loads the dictionary from the provided file or "DefaultDictionary.txt" if empty file name provided.
+		/// </summary>
+		/// <param name="fileName">File name.</param>
+		private static void LoadDictionary(string fileName)
+		{
+			System.IO.TextReader tr = null;
+			try
+			{
+				if(string.IsNullOrEmpty(fileName))
+				{
+					fileName = "DefaultDictionary.txt";
+				}
+
+				tr = (System.IO.TextReader)System.IO.File.OpenText (fileName);
+				if(tr != null)
+				{
+					string aLine = string.Empty;
+					do
+					{
+						aLine = tr.ReadLine ();
+						if(!string.IsNullOrEmpty(aLine))
+						{
+							msDictionary.Add(aLine.ToLower());
+						}
+					}
+					while (aLine != null);
+				}
+			}
+			catch(System.IO.FileNotFoundException fileNotFoundException)
+			{
+				Console.WriteLine (fileNotFoundException.Message);
+			}
+			catch(System.IO.FileLoadException fileLoadException)
+			{
+				Console.WriteLine(fileLoadException.Message);
+			}
+			catch(Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+			finally
+			{
+				if(tr != null)
+				{
+					tr.Close();
+				}
+			}
+		}
+
+		// TODO: make it read a JSON file or from DB
+		public static Graph MakeGraph()
+		{
+			#region Make graph
+			Graph g = new Graph ();
+
+			// Create and add nodes
+			Node p = new Node ('p');
+			g.AddNode (p);
+			Node o = new Node ('o');
+			g.AddNode (o);
+			Node p2 = new Node ('p');
+			g.AddNode (p2);
+			Node c = new Node ('c');
+			g.AddNode (c);
+			Node o2 = new Node ('o');
+			g.AddNode (o2);
+			Node r = new Node ('r');
+			g.AddNode (r);
+			Node n = new Node ('n');
+			g.AddNode (n);
+			Node a = new Node ('a');
+			g.AddNode (a);
+
+			// Add edges
+			//p
+			g.AddEdge(p, o);
+			g.AddEdge(p, a);
+			g.AddEdge(p, n);
+			g.AddEdge(p, r);
+			// o
+			g.AddEdge( o, p2);
+			g.AddEdge( o, a);
+			g.AddEdge( o, n);
+			g.AddEdge( o, p);
+			// p2
+			g.AddEdge( p2, c);
+			g.AddEdge( p2, a);
+			g.AddEdge( p2, o);
+			// c
+			g.AddEdge( c, o2);
+			g.AddEdge( c, n);
+			g.AddEdge( c, a);
+			g.AddEdge( c, p2);
+			// o2
+			g.AddEdge( o2, r);
+			g.AddEdge( o2, n);
+			g.AddEdge( o2, a);
+			g.AddEdge( o2, c);
+			// r
+			g.AddEdge( r, p);
+			g.AddEdge( r, n);
+			g.AddEdge( r, o2);
+			// n
+			g.AddEdge( n, p);
+			g.AddEdge( n, o);
+			g.AddEdge( n, a);
+			g.AddEdge( n, c);
+			g.AddEdge( n, o2);
+			g.AddEdge( n, r);
+			// a
+			g.AddEdge( a, o);
+			g.AddEdge( a, p2);
+			g.AddEdge( a, c);
+			g.AddEdge( a, o2);
+			g.AddEdge( a, n);
+			g.AddEdge( a, p);
+			#endregion Make graph
+
+			return g;
+		}
+
+		public static void Main (string[] args)
+		{
+			List<string> result = new List<string> ();
+
+			// Load the dictionary of known words
+			string dicFileName = args != null && args.Length >= 1 ? args [0] : string.Empty;
+			LoadDictionary (dicFileName);
+
+			// Make the graph
+			Graph g = MakeGraph ();
+
+			// Get the list of char combinations as potential words
+			List<string> combinations = g.GetCombinations();
+
+			// Check each combination we found (and print just for review)
+			Console.WriteLine ("Letter combinations found (may duplicate):");
+			foreach (string combination in combinations) {
+				if (IsWord (combination.ToLower()) && !result.Contains(combination)) {
+					result.Add (combination);
+				}
+
+				Console.Write (combination + "; ");
+			}
+
+			Console.WriteLine ();
+			Console.WriteLine ();
+			Console.WriteLine ("Here are the word we've found");
+			Console.WriteLine ();
+
+			// Print out the result
+			result.Sort ();
+			foreach (string word in result) {
+				Console.WriteLine (word);
+			}
+
+			Console.WriteLine ();
+			Console.WriteLine ();
+			Console.WriteLine ("Done! (Press any key...)");
+			Console.ReadKey ();
+		}
+	}
+}
